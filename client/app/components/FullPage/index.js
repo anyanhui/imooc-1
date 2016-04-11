baa44129431b1cf9f5b28a4ed85dc67853e7d8f6
Administrator <350346750@qq.com>
2016-04-12 01:13:58 +0800

    fullpage

--- client/app/components/FullPage/index.js
+++ client/app/components/FullPage/index.js
@@ -13,6 +13,11 @@ class FullPage extends Component {
     static Item = Item;
 
     pageScroll(e) {
+        let time = this.time;
+        let now = new Date().getTime();
+        if ((now - time) < 500) {
+            return;
+        }
         let y = e.deltaY;
         let count = Children.count(this.props.children);
         let active = this.state.active;
@@ -28,21 +33,24 @@ class FullPage extends Component {
     }
 
     render() {
+        this.time = new Date().getTime();
         const {children}=this.props;
         const active = this.state.active;
         return (
             <div className={styles.container}
                  onWheel={(e)=>this.pageScroll(e)}>
-                {
-                    Children.map(children, (item, i)=> {
-                        return (
-                            <div className={cls({
+                <div className={styles.item_container} style={{top:`-${active*100}%`}}>
+                    {
+                        Children.map(children, (item, i)=> {
+                            return (
+                                <div className={cls({
                                 [styles.item]:true,
                                 [styles.item_active]:i===active
                             })}>{item}</div>
-                        )
-                    })
-                }
+                            )
+                        })
+                    }
+                </div>
             </div>
         )
     }
